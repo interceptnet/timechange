241e3cb7e475959ce8bf66965789cdfbbfb2a564
Steven Sheffey <stevensheffey4@gmail.com>
2017-02-18 20:12:06 -0600

    Fixed alignment and normalized quotation marks on setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
 
 setup(
-    name='TimeChange',
-    version='0.1dev',
+    name="TimeChange",
+    version="0.1dev",
     description="Timechange is a library for analysis of timeseries data using convolutional neural nets",
-    author=', '.join(['Steven Sheffey',
-                        'John Ford']),
-    author_email=', '.join(['stevensheffey4@gmail.com']),
-    packages=['timechange',],
-    license='MIT',
-    long_description=open('README.rst','r').read(),
+    author=", ".join(["Steven Sheffey",
+                      "John Ford"]),
+    author_email=", ".join(["stevensheffey4@gmail.com"]),
+    packages=["timechange",],
+    license="MIT",
+    long_description=open("README.rst","r").read(),
 )
