c053be890d83a8bfb1287fbca50994a0ea980543
Steven Sheffey <stevensheffey4@gmail.com>
2017-02-18 20:01:04 -0600

    Modified setup script, added some attribution

--- setup.py
+++ setup.py
@@ -3,7 +3,11 @@ from distutils.core import setup
 setup(
     name='TimeChange',
     version='0.1dev',
+    description="Timechange is a library for analysis of timeseries data using convolutional neural nets",
+    author=', '.join(['Steven Sheffey',
+                        'John Ford']),
+    author_email=', '.join(['stevensheffey4@gmail.com']),
     packages=['timechange',],
-    license='',
-    long_description=open('README.rst').read(),
+    license='MIT',
+    long_description=open('README.rst','r').read(),
 )
